e9bcd7679011bec9ba7a5ca789314d6255268f6d
Virjanand <virjanand@virjanand.com>
2019-02-16 05:08:09 +0100

    Added show and hide logic behind show and hide buttons

--- app/src/main/java/com/virjanand/showhide/MainActivity.java
+++ app/src/main/java/com/virjanand/showhide/MainActivity.java
@@ -2,12 +2,26 @@ package com.virjanand.showhide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+
+    public void showText(View view) {
+        text.setVisibility(View.VISIBLE);
+    }
+
+    public void hideText(View view) {
+        text.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.surpriseText);
     }
 }
